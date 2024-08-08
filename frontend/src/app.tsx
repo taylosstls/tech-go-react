0d6eb0e6312db37f6bc48732a95d8bcc0858ea42
Gustavo Teixeira <luisgustavogto@gmail.com>
2024-08-08 15:17:50 -0300

    Create frontend structure

---
+++ frontend/src/app.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { CreateRoom } from "./pages/CreateRoom";
+import { Room } from "./pages/Room";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CreateRoom />,
+  },
+  {
+    path: "/room/:roomId",
+    element: <Room />,
+  },
+]);
+
+export function App() {
+  return <RouterProvider router={router} />;
+}
